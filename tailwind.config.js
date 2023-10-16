408b512fa342b77a876c8a297ebb8a82717ded18
MiguelEduardoH3 <109321183+MiguelEduardoH3@users.noreply.github.com>
2023-10-15 19:36:42 -0600

    Upload Documents

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["index.html", "./src/**/*.jsx"], 
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+// ** Todos los archivos de la carpeta correspondiente
+// *.jsx Busca todos los archivos que tengan la extensión .jsx
